1590e4c9706c636a84e51f099a37cf128d49eb42
vishnu <vishnu@descpro.com>
2022-03-15 16:59:41 +0530

    Changes in Naive-UI

--- naiveui-v3/src/components/HelloWorld.vue
+++ naiveui-v3/src/components/HelloWorld.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: {type:String,required:false} }>()
 
 const count = ref(0)
+const variants=["primary","secondary","success","error","warning","info","light","dark"]
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-<n-button tag="a" type="primary" href="https://next.antdv.com/index" target="_blank">Naive UI For Vue</n-button>
+  <h2>Buttons</h2>
+  <!-- loop n-button with variants -->
+  <n-button v-for="(variant,index) in variants" :key="'btn-variant'+index"  :type="variant" @click="count++">{{count}}</n-button>
+  <hr>
+<n-button tag="a" type="primary" href="https://www.naiveui.com/en-US/os-theme/docs/introduction" target="_blank">Naive UI For Vue</n-button>
 <hr/>
 <div>
   <n-button>Get Started</n-button>
